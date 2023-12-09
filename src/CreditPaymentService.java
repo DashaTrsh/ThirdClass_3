3c48e7e10892309cc99c2b494788f09448b080f0
dasha T <olga061200@icloud.com>
2023-12-09 14:44:22 +0300

    new formula

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,6 +1,6 @@
 public class CreditPaymentService {
-    public double calculate (int sum, double intRate, int timeMonths) {
-        double payment = (sum * intRate + sum ) / timeMonths;
+    public double calculate(int sum, double intRate, int timeMonths) {
+        double payment = (sum * intRate / 12) * Math.pow((1 + intRate / 12), timeMonths) / (Math.pow((1 + intRate / 12), timeMonths) - 1);
         int wholePayment = (int) payment;
         return wholePayment;
     }
--- src/Third.java
+++ src/Third.java
@@ -1,18 +1,18 @@
 public class Third {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
-
+// забавная у аннуитетного платежа формула))))
         System.out.println();
         System.out.println("Ежемесячный платеж при сроке 1 год равен ");
-        System.out.println(service.calculate(1_000_000,0.999, 12));
+        System.out.println(service.calculate(1_000_000, 0.0999, 12));
 
         System.out.println();
         System.out.println("Ежемесячный платеж при сроке 2 года равен ");
-        System.out.println(service.calculate(1_000_000,0.999, 24));
+        System.out.println(service.calculate(1_000_000, 0.0999, 24));
 
         System.out.println();
         System.out.println("Ежемесячный платеж при сроке 3 года равен ");
-        System.out.println(service.calculate(1_000_000,0.999, 36));
+        System.out.println(service.calculate(1_000_000, 0.0999, 36));
 
     }
 }
